7ce0c2876923978b85441420ea8ec40693f2bbbf
ndtrung1307 <ndtrung1307@gmail.com>
2019-07-07 23:33:04 +0700

    fix bug auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,7 +6,11 @@ module.exports = {
     createToken: async (user) => {
         // Sign the JWT
         return jwt.sign({
-            id: user._id
+            _id: user._id,
+            email: user.email,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            phone: user.phone
         }, config.jwt_secret, {
             algorithm: 'HS256',
             expiresIn: "1h"
